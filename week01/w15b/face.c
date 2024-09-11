0564158c06b0d0668c5d7a815a112b70b791e562
MysticalFire1315 <joelchong03@gmail.com>
2024-09-11 17:05:12 +1000

    Add initial week 1 code

---
+++ week01/w15b/face.c
@@ -0,0 +1,20 @@
+// face.c
+//
+// Written by YOUR-NAME (YOUR-ZID),
+// on TODAYS-DATE 
+//
+// The program prints two simple faces
+//
+// Face 1:
+//   ~ ~
+//  0 0
+//   o
+//   -
+//
+// Face 2:
+//  ~ ~
+//  0 0
+//   o
+//  \_/
+
+
